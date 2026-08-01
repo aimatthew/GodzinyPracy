558537f7ccc37394b398aca9db3fc4154ad9a88d
aimatthew <aimatthew@users.noreply.github.com>
2026-08-01 13:17:26 +0200

    Prepare release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "pl.godzinypracy.workly"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         buildConfigField("String", "GITHUB_REPOSITORY", "\"$githubRepository\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
